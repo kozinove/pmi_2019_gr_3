5dea6f0eeef86f68861c7b7fb9d2e6cb2ff3923b
Maxim Tsvetkov <unnlobach@gmail.com>
2020-04-13 23:41:04 +0300

    Lab

---
+++ Project3/core/main.cpp
@@ -0,0 +1,54 @@
+#include "CList.h"
+#include<iostream>
+#include"Parser.h"
+#include"CListsort.h"
+#include"Polinom.h"
+using namespace std;
+int main() {
+
+	// CList a;
+	// a.addF(2, 5, 1, 3);
+	// a.addF(1, 1, 2, 3);
+	// a.addF(5, 1, 10, 2);
+	// a.showList();
+	// CListsort b(a.getH());
+	// CListsort b.showList();
+	
+
+	//x^2*y^3*z^4+2*x^2*y*z^3
+
+	CList L; 
+	CList L2;
+	string s; 
+	string s2;
+	cin >> s; 
+	cin >> s2;
+	Parser s1(s);
+	Parser s3(s2);
+	s1.showst(); 
+	s3.showst();
+	s1.toList(L);
+	s3.toList(L2);
+	L.showList();
+	L2.showList();
+
+
+	CListsort L1(L.getH()); 
+	CListsort L3(L2.getH());
+	CList res(L1.toList());
+	CList res1(L3.toList());
+	Polinom P(res);
+	Polinom P1(res1);
+
+	P = P + P1;//не работает CList index не знаю почему(((
+	CList List(P.getL());
+	List.showList();
+	
+	//int R = P.CalculPol(2, 2, 2);
+	//cout << R;
+	
+	//с помощью итератора вывести полином
+	//перегрузить операции для полинома
+	return 0;
+
+}
