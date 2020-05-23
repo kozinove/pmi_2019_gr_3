7a3187d21a6ed2c5dde4b5c7fa25128a852dfff0
himomx'jfoaiefngbew'ofpwdqw[dlqwp[dlqwpd[wdlwqdqwld'dsl;agit add mp2lab4polygit config --global user.name himomxjfoaiefngbewofp <starkin-mh@list.ru>
2020-05-23 23:52:15 +0300

    done done

--- mp2lab4poly/MyLib/MyLib.cpp
+++
@@ -1,5 +0,0 @@
-﻿// MyLib.cpp : Определяет функции для статической библиотеки.
-//
-
-#include "pch.h"
-#include "framework.h"
--- mp2lab4poly/MyLib/monom.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-struct monom {
-	int c;
-	int d;
-	monom *next;
-};
